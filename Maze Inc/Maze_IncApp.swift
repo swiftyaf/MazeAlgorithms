0e98af9acca93820dce7c54b29fc35e721d1e489
Dimi Chakarov <dimitar@dchakarov.com>
2025-03-23 15:24:44 +0000

    Initial Commit

---
+++ Maze Inc/Maze_IncApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maze_IncApp.swift
+//  Maze Inc
+//
+//  Created by Dimi Chakarov on 23/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Maze_IncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Maze IncTests/Maze_IncTests.swift
@@ -0,0 +1,16 @@
+//
+//  Maze_IncTests.swift
+//  Maze IncTests
+//
+//  Created by Dimi Chakarov on 23/03/2025.
+//
+
+import Testing
+
+struct Maze_IncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
